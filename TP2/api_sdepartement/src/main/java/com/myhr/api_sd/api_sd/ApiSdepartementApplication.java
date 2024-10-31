b3d4f0a4117dd95630b9d6f7d12584ef551a3d70
asmaembr <asmae.moubr@gmail.com>
2024-10-31 17:45:27 +0100

    api departement created

---
+++ TP2/api_sdepartement/src/main/java/com/myhr/api_sd/api_sd/ApiSdepartementApplication.java
@@ -0,0 +1,13 @@
+package com.myhr.api_sd.api_sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSdepartementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSdepartementApplication.class, args);
+    }
+
+}
---
+++ TP2/api_sdepartement/src/test/java/com/myhr/api_sd/api_sd/ApiSdepartementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhr.api_sd.api_sd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSdepartementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

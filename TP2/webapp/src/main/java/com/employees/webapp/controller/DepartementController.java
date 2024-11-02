5273e429fb539a743a06396c95949376e4ed5fea
asmaembr <asmae.moubr@gmail.com>
2024-11-02 13:12:52 +0100

    update

--- TP2/webapp/src/main/java/com/employees/webapp/controller/DepartementController.java
+++ TP2/webapp/src/main/java/com/employees/webapp/controller/DepartementController.java
@@ -1,15 +1,48 @@
 package com.employees.webapp.controller;
 
-import com.employees.webapp.repository.DepartementProxy;
-import lombok.Data;
+import com.employees.webapp.model.Departement;
+import com.employees.webapp.model.Employee;
+import com.employees.webapp.service.DepartementService;
+import com.employees.webapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Data
-@Service
+@Controller
+@RequestMapping("/departements")
 public class DepartementController {
+
     @Autowired
-    private DepartementProxy departementProxy;
+    private DepartementService service ;
+
+    @GetMapping()
+    public String departements(Model model) {
+        Iterable<Departement> listdepartement = service.getDepartements();
+        model.addAttribute("departements", listdepartement);
+        return "departement";
+    }
+
+    @PostMapping("/deleteDepartement/{id}")
+    public String deleteDepartement(@PathVariable("id") final Long id) {
+        service.deleteDepartement(id);
+        return "redirect:/departements";
+    }
+
+    @PostMapping("/saveDepartement")
+    public String saveDepartement(@ModelAttribute Departement departement) {
+        service.saveDepartement(departement);
+        return "redirect:/departements";
+    }
+    @GetMapping({"/createDepartement/{id}", "/createDepartement"})
+    public String createDepartement(@PathVariable(required = false) Long id, Model model) {
+        if (id != null) {
+            model.addAttribute("departement", service.getDepartement(id));
+        } else {
+            model.addAttribute("departement", new Departement());
+        }
+        return "FormDepartement";
+    }
 
 
 
--- TP2/webapp/src/main/java/com/employees/webapp/service/DepartementService.java
+++ TP2/webapp/src/main/java/com/employees/webapp/service/DepartementService.java
@@ -1,4 +1,39 @@
 package com.employees.webapp.service;
 
+import com.employees.webapp.model.Departement;
+import com.employees.webapp.repository.DepartementProxy;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
 public class DepartementService {
+
+    @Autowired
+    private DepartementProxy departementProxy;
+
+    public Departement getDepartement(final Long id) {
+        return departementProxy.getDepartement(id);
+    }
+    public Iterable<Departement> getDepartements() {
+        return departementProxy.getDepartements();
+    }
+
+    public void deleteDepartement(final Long id) {
+        departementProxy.deleteDepartement(id);
+    }
+
+    public Departement saveDepartement(Departement departement){
+        Departement savedDepartement;
+        departement.setDescription(departement.getDescription().toUpperCase());
+        if(departement.getId()==null){
+            savedDepartement = departementProxy.saveDepartement(departement);
+        }
+        else {
+            savedDepartement = departementProxy.updateDepartement(departement);
+        }
+        return savedDepartement;
+    }
+
 }

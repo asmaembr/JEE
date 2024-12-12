435326c1686392fbc6061932cad0e960763105c6
asmaembr <asmae.moubr@gmail.com>
2024-12-12 15:49:17 +0100

    changes in product service

--- OpenFeign/microservice-produits/src/main/java/com/clientui/microserviceproduits/repositories/PaiementRepository.java
+++ OpenFeign/microservice-produits/src/main/java/com/clientui/microserviceproduits/repositories/PaiementRepository.java
@@ -2,6 +2,9 @@ package com.clientui.microserviceproduits.repositories;
 
 import com.clientui.microserviceproduits.models.Paiement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaiementRepository extends JpaRepository<Paiement, Long> {
+    Paiement findPaiementByClientName(String clientName);
 }

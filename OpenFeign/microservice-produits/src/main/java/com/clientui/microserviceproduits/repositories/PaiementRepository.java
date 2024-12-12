be128c7bebdb47441aab24e2ba0e8b71b88b4679
asmaembr <asmae.moubr@gmail.com>
2024-12-12 15:55:18 +0100

    PaiementRepository changes

--- OpenFeign/microservice-produits/src/main/java/com/clientui/microserviceproduits/repositories/PaiementRepository.java
+++ OpenFeign/microservice-produits/src/main/java/com/clientui/microserviceproduits/repositories/PaiementRepository.java
@@ -4,7 +4,9 @@ import com.clientui.microserviceproduits.models.Paiement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PaiementRepository extends JpaRepository<Paiement, Long> {
-    Paiement findPaiementByClientName(String clientName);
+    List<Paiement> findPaiementByClientName(String clientName);
 }
--- OpenFeign/microservice-produits/src/main/java/com/clientui/microserviceproduits/services/PaiementService.java
+++ OpenFeign/microservice-produits/src/main/java/com/clientui/microserviceproduits/services/PaiementService.java
@@ -1,11 +1,26 @@
 package com.clientui.microserviceproduits.services;
 
+import com.clientui.microserviceproduits.models.Paiement;
 import com.clientui.microserviceproduits.repositories.PaiementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PaiementService {
     @Autowired
     private PaiementRepository paiementRepository;
+
+    public List<Paiement> findAll() {
+        return paiementRepository.findAll();
+    }
+
+    public List<Paiement> findByClientName(String name) {
+        return paiementRepository.findPaiementByClientName(name);
+    }
+
+    public void save(Paiement paiement) {
+          paiementRepository.save(paiement);
+    }
 }
